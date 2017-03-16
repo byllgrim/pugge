c91acd56d533527756bdeecdb34b3be2ab1ca0f0
byllgrim <robin.a.t.pedersen@gmail.com>
2017-03-16 21:35:16 +0100

    add main function with a couple of TODOs

---
+++ pugge.c
@@ -0,0 +1,16 @@
+int
+main(void)
+{
+	/* TODO:
+	 * Open and parse files.
+	 * Take answer choice as input.
+	 * Present correct answer.
+	 * Create struct to hold questions with answers.
+	 * Take multiple files as argument.
+	 * Optional randomization of sequence.
+	 * Allow for variable number of answer choices.
+	 * Make a demo file.
+	 */
+
+	return 0;
+}
