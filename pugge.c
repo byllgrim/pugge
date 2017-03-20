4b2958d17f3619ed79d96d0d489ff5feb2189b8b
byllgrim <robin.a.t.pedersen@gmail.com>
2017-03-20 09:59:52 +0100

    add more TODO

--- pugge.c
+++ pugge.c
@@ -7,9 +7,12 @@ int main(void)
 	 * Present correct answer.
 	 * Create struct to hold questions with answers.
 	 * Take multiple files as argument.
-	 * Optional randomization of sequence.
+	 * Optional randomization of question sequence.
+	 * Randomization of answer order.
 	 * Allow for variable number of answer choices.
+	 * Allow #comments in question file.
 	 * Make a demo file.
+	 * Allow "Don't know" as an option?
 	 */
 
 	return 0;
